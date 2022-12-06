64ff9c66472f0398c5134eb2458eb31fadbf9d10
Paul van Buuren <paul@wbvb.nl>
2022-12-06 18:00:31 +0100

    updated version number

--- README.md
+++ README.md
@@ -3,9 +3,10 @@ Plugin voor het aanmaken van de 'thema'-taxonomie
 
 
 ## Current version:
-* 1.0.2 - Added English translation files.
+* 1.1.1 - Moved page templates from theme to this plugin. 
 
 ## Version history
+* 1.1.1 - Moved page templates from theme to this plugin. 
 * 1.0.2 - Added English translation files.
 * 1.0.1 - First working version.
 * 0.0.1 - Initial version.
--- ictuwp-plugin-thema-taxonomie.php
+++ ictuwp-plugin-thema-taxonomie.php
@@ -8,8 +8,8 @@
  * Plugin Name:         ICTU / Gebruiker Centraal / Thema taxonomie
  * Plugin URI:          https://github.com/ICTU/ictuwp-plugin-thema-taxonomie
  * Description:         Plugin voor het aanmaken van de 'thema'-taxonomie
- * Version:             1.0.2
- * Version description: Added English translation files.
+ * Version:             1.1.1
+ * Version description: Moved page templates from theme to this plugin. 
  * Author:              Paul van Buuren
  * Author URI:          https://github.com/ICTU/ictuwp-plugin-thema-taxonomie/
  * License:             GPL-2.0+
