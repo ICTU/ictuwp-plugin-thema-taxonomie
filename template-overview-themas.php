c57429cf3ae1a4f9493c9a9c353abb9e68286c7b
Paul van Buuren <paul@wbvb.nl>
2023-01-18 15:11:31 +0100

    use URL from thema_taxonomy_page if available and append thema class to card

--- template-overview-themas.php
+++ template-overview-themas.php
@@ -30,19 +30,25 @@ if ( function_exists( 'fn_ictu_thema_get_thema_terms' ) ) {
 
 	foreach ( fn_ictu_thema_get_thema_terms() as $thema ) {
 		$taxonomy = get_taxonomy( $thema->taxonomy );
-		$term_url = get_term_link( $thema );
+
+		if ( $thema->thema_taxonomy_page ) {
+			// a special page is available for this term
+			$term_url = $thema->thema_taxonomy_page;
+		} else {
+			// just use the term link
+			$term_url = get_term_link( $thema );
+		}
 
 		$item = array(
-			'type'  => 'thema',
-			'title' => $thema->name,
-			'descr' => $thema->description,
-			// IGNORE custom page url for now..
-			// 'url'   => $thema->thema_taxonomy_page,
-			'url'   => $term_url
+			'type'        => 'thema',
+			'title'       => $thema->name,
+			'descr'       => $thema->description,
+			'url'         => $term_url,
 		);
 
 		if ( $thema->thema_taxonomy_image ) {
-			$item['img'] = '<img src="' . $thema->thema_taxonomy_image['sizes']['image-16x9'] . '" alt=""/>';
+			// this term has a color scheme (called 'thema_taxonomy_image')
+			$item['thema'] = $thema->thema_taxonomy_image;
 		}
 
 		$context['overview']['items'][] = $item;
