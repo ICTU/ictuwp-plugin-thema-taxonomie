e8a0096c002af93ef5ba65e9e5d648d7e11ba344
David Hund <david.hund@gmail.com>
2023-01-26 15:01:12 +0100

    Update version.

--- ictuwp-plugin-thema-taxonomie.php
+++ ictuwp-plugin-thema-taxonomie.php
@@ -8,8 +8,8 @@
  * Plugin Name:         ICTU / Gebruiker Centraal / Thema taxonomie
  * Plugin URI:          https://github.com/ICTU/ictuwp-plugin-thema-taxonomie
  * Description:         Plugin voor het aanmaken van de 'thema'-taxonomie
- * Version:             1.2.3
- * Version description: First draft for thema detail page.
+ * Version:             1.2.4
+ * Version description: Update metabox-cta
  * Author:              Paul van Buuren
  * Author URI:          https://github.com/ICTU/ictuwp-plugin-thema-taxonomie/
  * License:             GPL-2.0+
